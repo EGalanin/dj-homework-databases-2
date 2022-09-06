a9c35ee69bc7ae759c7bd0a0076a7e6224121399
Evgeniy Galanin <galanin09234@mail.ru>
2022-09-06 17:33:01 +0300

    first commit

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -5,27 +5,27 @@ from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
 
-
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        counter = 0
         for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-            form.cleaned_data
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            raise ValidationError('Тут всегда ошибка')
-        return super().clean()  # вызываем базовый код переопределяемого метода
+            for key, value in form.cleaned_data.items():
+                if key == 'is_main' and value is True:
+                    counter += 1
+        if counter > 1:
+            raise ValidationError('Основной раздел может быть только один.')
+
+        return super().clean()
 
 
 class RelationshipInline(admin.TabularInline):
     model = Relationship
     formset = RelationshipInlineFormset
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    inline = [RelationshipInline]
+    inlines = [RelationshipInline]
     list_display = ('id', 'title', 'text', 'published_at', 'image')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'text')
