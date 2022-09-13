d413551daa88d44a9c4555722f904ccfd4dd47d2
Evgeniy Galanin <galanin09234@mail.ru>
2022-09-13 18:00:58 +0300

    first commit

--- drf-intro/simple_crud/measurements/views.py
+++ drf-intro/simple_crud/measurements/views.py
@@ -9,11 +9,6 @@ class SensorsView(ListAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
 
-    # def post(self, request):
-    #     if 'name' in request.data and 'description' in request.data:
-    #         new_sensor = Sensor(name=request.data['name'], description=request.data['description']).save()
-    #     return Response({'status': f"sensor {request.data['name']} - successfully registered!"})
-
 
 class SensorUpdateView(UpdateAPIView):
     queryset = Sensor.objects.all()
@@ -28,15 +23,6 @@ class SensorAPIDetailView(RetrieveUpdateDestroyAPIView):
 class MeasurementView(ListCreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
-
-    # def post(self, request, pk, *args, **kwargs):
-    #     if Sensor.objects.all().filter(id=pk).exists():
-    #         current_sensor = Sensor.objects.get(id=pk)
-    #         temperature = Measurement(temperature=request.data['temperature']).save()
-    #         current_sensor.measurements.append(temperature)
-    #         return Response({'status': f'Ok'})
-    #     else:
-    #         return Response({'status': f'сенсор не существует'})
 
 
 class MeasurementDetailView(ListCreateAPIView):
