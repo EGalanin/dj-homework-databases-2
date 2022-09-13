3511f3373c4191052254aa11fe9f9918c563f0c7
Evgeniy Galanin <galanin09234@mail.ru>
2022-09-13 17:59:04 +0300

    first commit

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -6,10 +6,10 @@ from django.db import models
 class Sensor(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=255, verbose_name='Описание датчика')
-    measurements = models.ManyToManyField('Measurement', related_name='measurements')
 
 
 class Measurement(models.Model):
     temperature = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    sens = models.ForeignKey(Sensor, on_delete=models.CASCADE, null=True, related_name='measurements')
--- drf-intro/simple_crud/measurements/views.py
+++ drf-intro/simple_crud/measurements/views.py
@@ -7,12 +7,12 @@ from .serializers import *
 
 class SensorsView(ListAPIView):
     queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
+    serializer_class = SensorDetailSerializer
 
-    def post(self, request):
-        if 'name' in request.data and 'description' in request.data:
-            new_sensor = Sensor(name=request.data['name'], description=request.data['description']).save()
-        return Response({'status': f"sensor {request.data['name']} - successfully registered!"})
+    # def post(self, request):
+    #     if 'name' in request.data and 'description' in request.data:
+    #         new_sensor = Sensor(name=request.data['name'], description=request.data['description']).save()
+    #     return Response({'status': f"sensor {request.data['name']} - successfully registered!"})
 
 
 class SensorUpdateView(UpdateAPIView):
@@ -22,21 +22,21 @@ class SensorUpdateView(UpdateAPIView):
 
 class SensorAPIDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
+    serializer_class = SensorDetailSerializer
 
 
 class MeasurementView(ListCreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
 
-    def post(self, request, pk, *args, **kwargs):
-        if Sensor.objects.all().filter(id=pk).exists():
-            current_sensor = Sensor.objects.get(id=pk)
-            temperature = Measurement(temperature=request.data['temperature']).save()
-            current_sensor.measurements.append(temperature)
-            return Response({'status': f'Ok'})
-        else:
-            return Response({'status': f'сенсор не существует'})
+    # def post(self, request, pk, *args, **kwargs):
+    #     if Sensor.objects.all().filter(id=pk).exists():
+    #         current_sensor = Sensor.objects.get(id=pk)
+    #         temperature = Measurement(temperature=request.data['temperature']).save()
+    #         current_sensor.measurements.append(temperature)
+    #         return Response({'status': f'Ok'})
+    #     else:
+    #         return Response({'status': f'сенсор не существует'})
 
 
 class MeasurementDetailView(ListCreateAPIView):
